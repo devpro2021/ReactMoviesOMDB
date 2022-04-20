170412b443d65591177b20ba32bec9db0cf35c38
aRyabintcev <aRyabintcev@mail.ru>
2022-04-20 20:37:57 +0300

    fix

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -3,20 +3,21 @@ import { Movies } from "../components/Movies";
 import Search from "../components/Search";
 import { Preloader } from "../components/Preloader";
 
+const API_KEY = process.env.REACT_APP_API_KEY;
 class Main extends React.Component {
   state = {
     movies: [],
     loading: true,
   };
   componentDidMount() {
-    fetch(`http://www.omdbapi.com/?apikey=${REACT_APP_API_KEY}&s=matrix`)
+    fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=matrix`)
       .then((response) => response.json())
       .then((data) => this.setState({ movies: data.Search, loading: false }));
   }
   searchMovies = (str, type = "all") => {
     this.setState({ loading: true });
     fetch(
-      `http://www.omdbapi.com/?apikey=${REACT_APP_API_KEY}&s=${str}${
+      `http://www.omdbapi.com/?apikey=${API_KEY}&s=${str}${
         type !== "all" ? `&type=${type}` : ""
       }`
     )
